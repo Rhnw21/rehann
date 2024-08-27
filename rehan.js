a24ab54e0fab1963e060b5d8e67b713f83fca057
Rhnw21 <166722865+Rhnw21@users.noreply.github.com>
2024-08-27 10:50:07 +0700

    update rehan.js

--- rehan.js
+++ rehan.js
@@ -45,7 +45,7 @@ export async function handler(store, chatUpdate) {
           let { performance } = (await import('perf_hooks')).default
           const start = performance.now()
           const latency = performance.now() - start
-          m.reply(`_Merespon dalam ${latency.toFixed(4)} detik_`)
+          m.reply(`_Merespon dalam: ${latency.toFixed(4)} detik_`)
           break
         case 'pull':
           let { execSync } = (await import('child_process')).default
